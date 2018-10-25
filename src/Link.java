14003436203c255bc1c063a9233ca12d98a8a150
Artoens <15076@ecam.be>
2018-10-25 16:15:52 +0200

    implement link class
    Signed-off-by: Artoens <15076@ecam.be>

--- src/Link.java
+++ src/Link.java
@@ -1,2 +1,29 @@
+import java.util.Set;
+
 public class Link {
+
+    public Set<Integer> getIds() {
+        return ids;
+    }
+
+    private Set<Integer> ids;
+
+    public int getCreated() {
+        return created;
+    }
+
+    private int created;
+
+    public Link(Set<Integer> ids){
+        this.ids = ids;
+    }
+
+    public Link(Set<Integer> ids, int created){
+        this.created = created;
+        this.ids = ids;
+    }
+
+    public boolean  compare(Link other){
+        return ids.equals(other.getIds());
+    }
 }
