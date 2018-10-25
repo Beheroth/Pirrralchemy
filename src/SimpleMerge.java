21fa4b248d7014eb74c0cd34a72fb3fcca109b8d
Artoens <15076@ecam.be>
2018-10-25 16:16:23 +0200

    first try to implement interpret method
    Signed-off-by: Artoens <15076@ecam.be>

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -1,19 +1,29 @@
-public class SimpleMerge extends NonTerminalExpression{
-    private MyElement firstelem;
-    private MyElement secondelem;
+import java.util.HashSet;
+import java.util.Set;
 
-    public SimpleMerge(MyElement firstelem, MyElement secondelem){
-        this.firstelem = firstelem;
-        this.secondelem = secondelem;
+public class SimpleMerge extends NonTerminalExpression{
+    private Set<MyElement> Elements;
+
+    public SimpleMerge(Set<MyElement> Elements){
+        this.Elements = Elements;
     }
 
     @Override
     public AbstractExpression interpret(){
-        return new MyElement( "Hello",  1);
+        Set<Integer> ids = new HashSet<Integer>();
+
+        for(MyElement elem : Elements){
+            ids.add(elem.getID());
+        }
+
+        Link fakeLink = new Link(ids);
+
+        Link foundLink = MyApp.getLinks().stream().filter(link -> fakeLink.compare(link)).findAny().orElseThrow();
+
+        MyElement out = MyApp.getUnknownElements().stream().filter(element -> element.getID() == foundLink.getCreated()).findAny().orElseThrow();
+
+        return out;
     }
-    /*
-    private String generateLinkAlias(){
-        return firstelem.getID() + secondelem.getID();
-    }
-    */
+
+
 }
