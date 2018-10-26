d28a9521954c798a70731bac245a3a7817ec110d
Artoens <15076@ecam.be>
2018-10-26 16:33:57 +0200

    add save methods
    Signed-off-by: Artoens <15076@ecam.be>

--- src/Context.java
+++ src/Context.java
@@ -1,9 +1,5 @@
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Context {
 
@@ -17,19 +13,22 @@ public class Context {
     private void create(){
 
         Set<MyElement> knownElements  = MyApp.getKnownElements();
-        System.out.println(knownElements);
-        Set<MyElement> myElements = new HashSet<MyElement>();
+        Set<MyElement> elementsToMerge = new HashSet<MyElement>();
         for (String word: words) {
             try {
-                myElements.add(knownElements.stream().filter(element -> word.equals(element.getName())).findAny().orElseThrow());
+                elementsToMerge.add(knownElements.stream().filter(element -> word.equals(element.getName())).findAny().orElseThrow());
             }
             catch (Exception e) {
                 System.out.println("Unknown element.");
             }
         }
 
-        for (MyElement elem : myElements) {
-            System.out.println(elem.getName());
-    }
+        SimpleMerge merge = new SimpleMerge(elementsToMerge);
+        MyElement out = (MyElement) merge.interpret();
+        if (out != null){
+            System.out.println("You created " + out.getName() + " !");
+            //MyApp.elementMerged(out);
+            MyApp.askIfWin();
+        }
     }
 }
