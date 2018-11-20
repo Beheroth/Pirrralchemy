855ea9c02549d2c08268f8056f4cd6be4cf2dc9f
Beheroth <arnaud.peeters95@gmail.com>
2018-11-20 10:42:10 +0100

    Create Test Classes
    These classes are needed for Junit tests

---
+++ src/test/MyAppTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyAppTest {
+
+    @Test
+    public void getKnownElements() {
+    }
+
+    @Test
+    public void getUnknownElements() {
+    }
+
+    @Test
+    public void getLinks() {
+    }
+
+    @Test
+    public void load() {
+    }
+
+    @Test
+    public void askIfWin() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void elementMerged() {
+    }
+}
